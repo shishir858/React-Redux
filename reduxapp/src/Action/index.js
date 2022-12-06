549b942a9b72e446330b622ff933ffbfd3edbe0d
shishir <hi4rgcshishir@gmail.com>
2022-12-06 22:15:17 +0530

    change aap.js and create to new folder Action/Reducer

---
+++ reduxapp/src/Action/index.js
@@ -0,0 +1,8 @@
+
+export const Increment =()=>{
+    type:"INCREMENT";
+}
+
+export const Decrement =()=>{
+    type:"DECREMENT";
+}
--- reduxapp/src/App.js
+++ reduxapp/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className='App-header'>
+      <h1>Increment/Decrement counter.</h1>
+      <h4>using React and Redux</h4>
+      <div className='quantity'>
+        <a className='quantity_minus' title='Decrement'><span>-</span></a>
+        <input name='quantity' type="text" className="quintity_input" value={0} disabled/>
+        <a className='quantity_minus' title='Decrement'><span>+</span></a>
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App

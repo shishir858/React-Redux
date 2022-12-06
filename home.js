c71f8de41181e3302c28c7fb97c70bed1b10a971
shishir <hi4rgcshishir@gmail.com>
2022-12-06 19:37:57 +0530

    second commit

---
+++ home.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>
+            Redux
+        </title>
+    </head>
+
+    <body>
+        <h1>Hello ! This is Redux Increment and Decerement</h1>
+    </body>
+</html>
